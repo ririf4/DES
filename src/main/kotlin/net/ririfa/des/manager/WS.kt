540e5f55fba56d9d0c10e42c8e8cc2bac6ce0dc3
RiriFa <main@ririfa.net>
2025-03-25 02:36:13 +0900

    update

--- src/main/kotlin/net/ririfa/des/manager/WS.kt
+++ src/main/kotlin/net/ririfa/des/manager/WS.kt
@@ -9,14 +9,9 @@ import io.netty.handler.codec.http.HttpObjectAggregator
 import io.netty.handler.codec.http.HttpServerCodec
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler
-import io.papermc.paper.event.player.PlayerChangeBeaconEffectEvent
 import net.ririfa.des.Logger
-import org.bukkit.Effect
-import org.bukkit.EntityEffect
 import org.bukkit.inventory.Inventory
-import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
-import org.bukkit.potion.PotionEffectTypeCategory
 
 class WS(
 	private val port: Int,
@@ -68,14 +63,14 @@ class WS(
 		workerGroup?.shutdownGracefully()
 	}
 
-	data class PlayerData(
-		val health: Int,
-		val effects: List<PotionEffectType>,
-		val inventory: Inventory,
-		val food: Int,
-		val hiddenFood: Int,
-		val experience: Float,
-		// 酸素ゲージ。型は分からん
-		val o2: Double
-	)
+    data class PlayerData(
+        val health: Int,
+        val effects: List<PotionEffectType>,
+        val inventory: Inventory,
+        val food: Int,
+        val hiddenFood: Int,
+        val experience: Float,
+        // 酸素ゲージ。型は分からん
+        val o2: Double
+    )
 }
